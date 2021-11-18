e67fef5ecebafe63f021c87f0d52e82420d0fcf1
John Feng <atjohnfeng@gmail.com>
2021-11-17 18:16:40 -0800

    Add translate method

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -12,6 +12,7 @@ public class Driver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 0, 45);
+        transform.Rotate(0, 0, 0.01f);
+        transform.Translate(0, 0.01f, 0);
     }
 }
