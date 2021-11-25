154bb5d790e51e9bffbbe849f4090569f01f5eb3
John Feng <atjohnfeng@gmail.com>
2021-11-24 18:19:48 -0800

    Add trigger debug message

--- Assets/Scripts/Collision.cs
+++ Assets/Scripts/Collision.cs
@@ -7,4 +7,8 @@ public class Collision : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D other) {
         Debug.Log("COLLISION DETECTED.");
     }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("YOU HAVE ENTERED THE VOID.");
+    }
 }
