ebda653a65fc2df84c9c30670da47b3b4e193b0d
John Feng <atjohnfeng@gmail.com>
2021-12-08 18:47:59 -0800

    Add additional trigger code for package and custoemr

---
+++ Assets/Scripts/Delivery.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delivery : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other) {
+        Debug.Log("COLLISION DETECTED.");
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        // Debug.Log("YOU HAVE ENTERED THE VOID.");
+
+        if (other.tag == "Package") {
+            Debug.Log("Package has been picked up.");
+        } else if (other.tag == "Customer") {
+            Debug.Log("Delivery location.");
+        }
+    }
+}
