16482bad58438918a8efa1d0723e44d40983e15a
John Feng <atjohnfeng@gmail.com>
2021-12-13 18:30:45 -0800

    Add space

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Delivery : MonoBehaviour
 {
     [SerializeField] Color32 hasPackageColor = new Color32 (1, 1, 1, 1);
-    [SerializeField] Color32 noPackageColor = new Color32(1, 1, 1, 1);
+    [SerializeField] Color32 noPackageColor = new Color32 (1, 1, 1, 1);
 
     [SerializeField] float destroyDelay = 0.5f;
     bool hasPackage;
