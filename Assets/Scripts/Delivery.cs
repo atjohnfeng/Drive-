5b1149a083e06c0b14eb19a83042ccc3bd2aa09e
John Feng <atjohnfeng@gmail.com>
2021-12-13 18:16:14 -0800

    Add addition bool

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -7,18 +7,14 @@ public class Delivery : MonoBehaviour
     [SerializeField] float destroyDelay = 0.5f;
     bool hasPackage;
 
-    private void Start() {
-        Debug.log(hasPackage);
-    }
-
     private void OnCollisionEnter2D(Collision2D other) {
         Debug.Log("COLLISION DETECTED.");
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
-        // Debug.Log("YOU HAVE ENTERED THE VOID.");
+        Debug.Log("YOU HAVE ENTERED THE VOID.");
 
-        if (other.tag == "Package") {
+        if (other.tag == "Package" && !hasPackage) {
             Debug.Log("Package has been picked up.");
             hasPackage = true;
             Destroy(other.gameObject, destroyDelay);
