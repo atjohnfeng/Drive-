e9f92ec430b6dcabd35e3bb32318afc5f2f0ae1e
John Feng <atjohnfeng@gmail.com>
2021-12-15 18:03:35 -0800

    Change default speed values and add boostSpeed variable

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class Driver : MonoBehaviour
 {
     [SerializeField] float steerSpeed = 0.5f;
-    [SerializeField] float moveSpeed = 0.01f;
-    [SerializeField] float slowSpeed = 0.1f;
+    [SerializeField] float moveSpeed = 20f;
+    [SerializeField] float slowSpeed = 15f;
+    [SerializeField] float boostSpeed = 30f;
     // Start is called before the first frame update
     void Start()
     {
